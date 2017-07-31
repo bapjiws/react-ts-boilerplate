13555d7dcfd54091b1a2c090f1966b6bcf957063
bapjiws <alex.boklin@gmail.com>
2017-07-31 11:18:18 +0300

    Integrate Prettier into ESLint (config needs to be cleaned)

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,56 @@
+// module.exports = {
+//     "extends": ["prettier", "prettier/react"], //,
+//
+//     "plugins": ["prettier", "react"], // , "react"
+//
+//     "parser": "typescript-eslint-parser", // babel-eslint
+//
+//     // http://eslint.org/docs/rules/
+//     // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+//     "rules": {
+//         "prettier/prettier": "error",
+//
+//         // "strict": 0,
+//         // "comma-dangle": 0,
+//         //
+//         // "react/jsx-filename-extension": ["error", {"extensions": [".jsx", ".tsx"]}],
+//         //
+//         // "import/extensions": ["error", "always", {
+//         //     "js": "never",
+//         //     "jsx": "never",
+//         //     "ts": "never",
+//         //     "tsx": "never"
+//         // }],
+//         //
+//         // "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
+//
+//     },
+//
+//     "settings": {
+//         "import/resolver": {
+//             "node": {
+//                 "extensions": [".js", ".jsx", ".ts", ".tsx"]
+//             }
+//         },
+//
+//         "import/core-modules": [ "autoprefixer" ],
+//     }
+// };
+
 module.exports = {
-    "extends": "airbnb", // https://www.npmjs.com/package/eslint-config-airbnb
-
-    "plugins": ["react"],
-
-    "parser": "typescript-eslint-parser",
-
-    // http://eslint.org/docs/rules/
-    // https://github.com/yannickcr/eslint-plugin-react/tree/master/docs/rules
+    "extends": [
+        "airbnb",
+        "plugin:react/recommended",
+        "prettier",
+        "prettier/react"
+    ],
+    "plugins": [
+        "react",
+        "prettier"
+    ],
+    "parser": "typescript-eslint-parser" ,
     "rules": {
-        "strict": 0,
-        "comma-dangle": 0,
+        "prettier/prettier":  ["error", {"singleQuote": true}],
 
         "react/jsx-filename-extension": ["error", {"extensions": [".jsx", ".tsx"]}],
 
@@ -21,9 +62,7 @@ module.exports = {
         }],
 
         "import/no-extraneous-dependencies": ["error", {"devDependencies": true}]
-
     },
-
     "settings": {
         "import/resolver": {
             "node": {
